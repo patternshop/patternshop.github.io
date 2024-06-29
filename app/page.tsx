864ab0ef538aa5d0ace1a6a0475554143793ddf0
Antoine Viellard <antoine.viellard@gmail.com>
2024-06-29 18:23:31 +0100

    Work in progress

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,22 @@
-import Image from "next/image";
 import MyNavbar from "./my_navbar";
+import Image from "next/image";
+import tissushor2 from "../public/tissushor2.jpg"
+
 export default function Home() {
   return (
     <main className="container max-w-3xl mx-auto rounded-md border-solid border-2 border-gray-200 m-5 p-5 bg-white">
       <MyNavbar />
-      <h1 className="text-blue-500">I'm blue!</h1>
+      <div>
+      What is Patternshop?
+      Patternshop is a powerful tool enabling the repetition of images in organisation of networks, creation of textures and realistic mapping.
+      More details
+
+      Who is Patternshop aimed at?
+
+      Patternshop is aimed at designers working in the infographic, textile design, interior design sectors....
+      More details
+      </div>
+      <Image src={tissushor2} className="w-auto h-auto" alt="Tissus" />
     </main>
   );
 }
